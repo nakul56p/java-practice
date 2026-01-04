080aec5da122caa501f043ea638e86f0ce90b007
nakul56p <nakul56p@gmail.com>
2026-01-04 16:27:13 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(Strings[] args) {
+    System.out.println("Hello world");
+    System.out.println("This is my first Java program");
+    System.out.println("Learning Java step by step");
+  }
+}
